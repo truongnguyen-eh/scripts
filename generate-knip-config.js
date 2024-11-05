320ad810c26ded389f7772348643be52f68e495b
Truong NT <truong.nguyen2@employmenthero.com>
2024-11-05 18:29:48 +0700

    Update latest used generate-knip-config script

--- generate-knip-config.js
+++ generate-knip-config.js
@@ -9,33 +9,7 @@ How to use this script?
 */
 
 import fs from 'fs';
-import path from 'path';
 import { exec } from 'child_process';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const walk = dir => {
-  let results = [];
-  const list = fs.readdirSync(dir);
-  list.forEach(file => {
-    file = `${dir}/${file}`;
-    const stat = fs.statSync(file);
-    if (stat && stat.isDirectory()) {
-      /* Recurse into a subdirectory */
-      results = results.concat(walk(file));
-    } else {
-      /* Is a file */
-      results.push(file);
-    }
-  });
-  return results;
-};
-
-function listFiles(dir, regex = /\.*/) {
-  return walk(dir).filter(f => regex.test(f));
-}
 
 // Step 1: Get list of modified files last 2 weeks
 const getModifiedFiles = () =>
@@ -56,68 +30,12 @@ const getModifiedFiles = () =>
     );
   });
 
-// Step 2: Get list of files that contain dynamic imports
-const toAbsoluteImport = (importPath, filePath, projectRoot) => {
-  // If the importPath is relative (starts with .), resolve it to an absolute path
-  if (importPath.startsWith('.')) {
-    return path.resolve(path.dirname(filePath), importPath);
-  }
-  // For non-relative paths, assume it's an absolute path from the project root
-  return path.resolve(projectRoot, importPath);
-};
-
-const extractDynamicImports = (filePath, projectRoot, dynamicImportRegex) => {
-  const fileContent = fs.readFileSync(filePath, 'utf8');
-  const dynamicImports = [];
-  let match;
-
-  while ((match = dynamicImportRegex.exec(fileContent)) !== null) {
-    // The second capture group is the module path in the import
-    const importPath = match[2];
-    const absoluteImportPath = toAbsoluteImport(
-      importPath,
-      filePath,
-      projectRoot
-    );
-    // Todo: Example: apps/hr-web-app/src/modules/ats/components/SalaryBenchmarkTool/VerifyFormModal
-    // -> apps/hr-web-app/src/modules/ats/components/SalaryBenchmarkTool/VerifyFormModal.tsx
-    // We need to check if [file].{ts,js} exist -> If not -> Move to index.{ts,js}
-    dynamicImports.push(absoluteImportPath);
-  }
-
-  return dynamicImports;
-};
-
-const scanFilesForDynamicImports = () => {
-  const projectRoot = path.resolve(__dirname);
-  const allDynamicImports = [];
-  const dynamicImportRegex =
-    /import\(\s*(\/\*.*?\*\/\s*)?['"`](.*?)['"`]\s*\)/gs;
-  const filesContainDynamicImports = listFiles(
-    `${projectRoot}/apps/hr-web-app/src/modules`,
-    /.*\.(js|jsx|ts|tsx)$/
-  );
-
-  filesContainDynamicImports.forEach(filePath => {
-    allDynamicImports.push(
-      ...extractDynamicImports(filePath, projectRoot, dynamicImportRegex)
-    );
-  });
-
-  return allDynamicImports;
-};
-
-// Step 3: Generate Knip config file
+// Step 2: Generate Knip config file
 const generateKnipConfigFile = async () => {
   const ignoreFiles = [
     ...new Set([
       ...(await getModifiedFiles()),
-      ...scanFilesForDynamicImports(),
       'apps/hr-web-app/src/modules/adminContract/components/AdminContract/translations.js', // getTranslations is removed wrongly by Knip
-      'apps/hr-web-app/src/modules/emailTemplates/components/RevisionContentInput/__mocks__/index.tsx',
-      'apps/hr-web-app/src/modules/emailTemplates/components/RevisionSubjectInput/__mocks__/index.tsx',
-      'apps/hr-web-app/src/modules/financialServices/components/HomeLoans/__mocks__/LoanMarketScript.ts',
-      'apps/hr-web-app/src/modules/spa/beta/components/Navigation/menuItems/__mocks__/index.ts'
     ]),
   ];
 
@@ -133,7 +51,7 @@ const generateKnipConfigFile = async () => {
           'src/keypay/index.ts',
           'src/agnosticEvents/index.js',
           'src/agnosticEvents/keypay.js',
-          '**__mocks__/**/*.js',
+          '**__mocks__/**/*.{js,ts,tsx}',
         ],
         project: '**/*.{js,ts,tsx}',
       },
